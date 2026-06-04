3c6902b3e106df881f7c027db0c8115a446f9724
Tonjr <wwffjr@gmail.com>
2026-06-04 12:28:30 -0300

    treino 1

---
+++ curso_em_video/MUNDO 3 PYTHON(DICIONARIOS)/ex.19(treino_a).py
@@ -0,0 +1,19 @@
+print('-=' * 20)
+print('         TREINANDO DICIONÁRIOS         ')
+print('-=' * 20)
+
+# =====================================================================
+# 🚗 TREINO 1: O Dicionário do Carro (Criação e Acesso)
+# Objetivo: Criar um dicionário chamado 'carro' com as chaves: 'marca', 'modelo' e 'ano'.
+# Depois, mostre uma f-string na tela usando esses dados.
+# =====================================================================
+print('\n--- Treino 1 ---')
+
+# 1. Crie o dicionário aqui com os dados do carro que você quiser:
+carro = {'marca':'chevrolet','modelo':'onix','ano':2026}
+print(carro['marca'])
+print(carro['modelo'])
+print(carro['ano'])
+print(f'Eu tenho um {carro["modelo"]} da {carro["marca"]} ano {carro["ano"]}.')
+
+
